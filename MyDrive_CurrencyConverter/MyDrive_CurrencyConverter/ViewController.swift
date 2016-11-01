e20d9ab6d8d31c99267554e64c10ccf2bcefae75
Robin Spinks <uglybluecat@googlemail.com>
2016-11-01 15:45:47 +0000

    created base project

---
+++ MyDrive_CurrencyConverter/MyDrive_CurrencyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDrive_CurrencyConverter
+//
+//  Created by Robin Spinks on 01/11/2016.
+//  Copyright © 2016 Robin Spinks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
